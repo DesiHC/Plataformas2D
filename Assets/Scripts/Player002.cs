b913c4f9b1bb46273da05a1ba5fc68d516db388e
DesiHC <green_flyer@hotmail.com>
2016-11-09 16:56:00 +0100

    Subida_Inicial
    Archivos con el proyecto inicial

---
+++ Assets/Scripts/Player002.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player002 : MonoBehaviour {
+	private Animator animator;
+	private Rigidbody2D rb;
+
+	void start (){
+		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("right"))
+			
+	
+	}
+}
---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+	Animator anim;
+	Rigidbody2D rigi;
+	public float fuerza = 1;
+	public float fsalto = 400;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> (); // Coger el Animator de el elemento que tenga el script
+		rigi = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetFloat ("velocidad", 1f);
+			rigi.AddForce (transform.right*fuerza);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			anim.SetFloat ("velocidad", 0f);
+
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			anim.SetFloat ("velocidad", 1f);
+			rigi.AddForce (transform.right*-fuerza);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			anim.SetFloat ("velocidad", 0f);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool  ("jump", true);
+			rigi.AddForce (transform.up*fsalto);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			anim.SetBool ("jump", true);
+		}
+	}
+}
