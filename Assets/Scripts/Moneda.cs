6351972e21d11c95370e09e9226476da307da380
DesiHC <green_flyer@hotmail.com>
2016-11-11 17:42:47 +0100

    Moneda y panel para sumar
    Hemos creado un panel que cuenta las monedas que vas cogiendo. También
    ha habido modificaciones de terreno

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Moneda : MonoBehaviour {
 	private Rigidbody2D rb;
+	GameObject txt_moneda;
+	control_monedas ctrl_moneda; 
+
 	// Use this for initialization
 	void Start () {
 		Destroy (gameObject,5);
 		rb = GetComponent<Rigidbody2D> ();
 		rb.AddForce (new Vector2(Random.Range (-100,100), 100));
+		txt_moneda = GameObject.Find ("texto_moneda");
+		ctrl_moneda = txt_moneda.GetComponent<control_monedas> ();
+
 	}
 	
 	// Update is called once per frame
@@ -16,8 +22,10 @@ public class Moneda : MonoBehaviour {
 	}
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
+			ctrl_moneda.suma_monedas (5);
 			Destroy (gameObject);
 			Random.Range (0,10);
+
 		}
 	}
 }
