8e9ae4e017f8b4f55e3e19ec927d910a33dc904b
DesiHC <green_flyer@hotmail.com>
2016-11-16 17:39:39 +0100

    Arreglo de scripts
    Arreglamos cámara y jugador

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,34 +1,34 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
 
 	public float interpVelocity;
 	public float minDistance;
-	public float followDistance;
+	public float followDistance; 
 	public GameObject target;
 	public Vector3 offset;
 	Vector3 targetPos;
+
+
 	// Use this for initialization
 	void Start () {
 		targetPos = transform.position;
 	}
-
+	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (target)
-		{
+		if (target) {
 			Vector3 posNoZ = transform.position;
 			posNoZ.z = target.transform.position.z;
 
 			Vector3 targetDirection = (target.transform.position - posNoZ);
 
-			interpVelocity = targetDirection.magnitude * 5f;
+			interpVelocity = targetDirection.magnitude * 40f;
 
-			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
 
-			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
-
+			transform.position = Vector3.Lerp (transform.position, targetPos + offset, 0.25f);
 		}
 	}
 }
