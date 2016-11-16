9b418e9dfbce70c4bb784a707a7d9823a9ac2748
DesiHC <green_flyer@hotmail.com>
2016-11-16 17:18:52 +0100

    Script para enlazar la plataforma

---
+++ Assets/Scripts/EnlazarPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(BoxCollider2D))] //Con esto añades un boxcollider a cualquier objeto que tenga este script
+
+public class EnlazarPlayer : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.parent = transform;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.parent = null;
+		}
+	}
+}
--- Assets/Scripts/MovimientoRecto.cs
+++ Assets/Scripts/MovimientoRecto.cs
@@ -6,19 +6,19 @@ public class MovimientoRecto : MonoBehaviour {
 	public Transform inicio;
 	public Transform fin;
 	public float velocidad = 10f;
-	private Vector3 destino;
+	private Vector3 destino; //variable?
 
 	// Use this for initialization
 	void Start () {
-		destino = fin.position; 
+		destino = fin.position; //Fin es un transform, tienes que especificar si quieres position, scale o rotation
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () { //Update se ejecuta una vez cada frame
 		plataforma.transform.position = 
 			Vector3.MoveTowards (plataforma.transform.position,
 				destino,
-				velocidad * Time.deltaTime);
+				velocidad * Time.deltaTime); //MoveTowards mueve algo entre dos puntos. en este caso "plataforma" se mueve desde su posición hasta destino.
 
 		if (plataforma.transform.position == fin.position) {
 			destino = inicio.position;
