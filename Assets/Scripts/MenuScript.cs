9f07d4ecee75e28449fc62a1a2b73718b8e4db77
DesiHC <green_flyer@hotmail.com>
2016-11-22 20:55:32 +0100

    Script para menu
    Terminamos la animación del menú y le añadimos un script que hace que al
    pulsar la tecla Escape, salte la pausa.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Animator))]
+
+public class MenuScript : MonoBehaviour {
+	Animator anim;
+	private bool menu_pausa = false;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (menu_pausa) {
+				continuar ();
+			} else {
+				pausa ();
+				}
+			}
+		}
+
+
+
+
+	public void pausa (){
+		menu_pausa = true;
+		Time.timeScale = 0;
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", true);
+	}
+
+	public void continuar (){
+		menu_pausa = false;
+		Time.timeScale = 1;
+		anim.SetBool ("opciones", false);
+		anim.SetBool ("pausa", false);
+	}
+
+	public void salir (){
+		Application.Quit ();
+		Debug.Log ("Saliendo");
+	}
+
+	public void opciones (){
+		anim.SetBool ("opciones", true);
+		anim.SetBool ("pausa", true);
+	}
+
+
+
+
+}
